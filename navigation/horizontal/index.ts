e2de25d803ec4819c7b5ef1b404127d26ffb2b1c
mahmod <mahmodmos47@gmail.com>
2025-03-16 20:08:18 +0200

    test

---
+++ navigation/horizontal/index.ts
@@ -0,0 +1,17 @@
+export default [
+  {
+    title: "Home",
+    to: { name: "index" },
+    icon: { icon: "tabler-smart-home" },
+  },
+  {
+    title: "Second page",
+    to: { name: "second-page" },
+    icon: { icon: "tabler-file" },
+  },
+  {
+    title: "test page",
+    to: { name: "test-page" },
+    icon: { icon: "tabler-file" },
+  },
+];
---
+++ pages/test-page.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <VCard title="Create Awesome 🙌">
+      <VCardText>This is your second page.</VCardText>
+      <VCardText>
+      test
+      </VCardText>
+    </VCard>
+  </div>
+</template>
