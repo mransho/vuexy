b6d4926afe039305644e08cd9d54bd9268443e22
mahmod <mahmodmos47@gmail.com>
2025-03-16 20:28:43 +0200

    test

--- pages/test-page.vue
+++ pages/test-page.vue
@@ -2,9 +2,23 @@
   <div>
     <VCard title="Create Awesome 🙌">
       <VCardText>This is your second page.</VCardText>
-      <VCardText>
-      test
-      </VCardText>
+      <VCardText> test </VCardText>
     </VCard>
   </div>
 </template>
+<script lang="ts" setup>
+import { definePage } from "vue";
+definePage({
+  meta: {
+    action: "read",
+    subject: "AclDemo",
+  },
+});
+
+const user = {
+  action: "read" as const,
+
+  // `subject` property type is `Subjects` ("src/plugins/casl/AppAbility.ts")
+  subject: "Admin" as const,
+};
+</script>
